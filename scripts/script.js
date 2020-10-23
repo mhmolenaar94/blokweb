96344c01375f8a39eccdd1bc98d26f3f176d2a5d
mhmolenaar94 <70691939+mhmolenaar94@users.noreply.github.com>
2020-10-23 12:03:04 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var deSter
+
+deSter = document.querySelector("boxster")
+deSter.addEventListener("click", veranderMezelf);
+
+function veranderMezelf() {
+  deSter.classList.add("boxsterclosed");
+}
